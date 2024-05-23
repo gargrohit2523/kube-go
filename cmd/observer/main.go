ac5e5a27cbd5ca77a924b80b2bcaf7f368bc6a7c
Rohit X1C <rohitgarg11@outlook.com>
2024-05-23 11:15:30 +0530

    add pkg/cmd structure with improved concurrency using waitGroup and context

--- cmd/observer/main.go
+++ cmd/observer/main.go
@@ -3,105 +3,61 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
+	"sync"
 
-	v1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/gargrohit2523/kube-observer/pkg/observer"
+
 	"k8s.io/client-go/util/homedir"
 )
 
-var done = make(chan struct{}, 1)
-var monitorStopped = make(chan struct{}, 1)
-
 func main() {
+
 	var ns *string = flag.String("ns", "default", "| separated namespaces")
 	var kubeconfig *string
+	var interval = flag.Int("interval", 5, "Time interval in minutes to monitor")
+
+	defaultKubeConfig := ""
 
 	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		defaultKubeConfig = filepath.Join(home, ".kube", "config")
 	}
+
+	kubeconfig = flag.String("kubeconfig", defaultKubeConfig, "Path to kube config file. By default picks from /home/.kubeconfig if available")
+
 	flag.Parse()
 
 	namespaces := strings.Split(*ns, "|")
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-
-	if err != nil {
-		panic(err)
-	}
+	var done = make(chan struct{}, 1)
 
 	go func() {
 		os.Stdin.Read(make([]byte, 1))
 		close(done)
 	}()
 
-	for _, namespace := range namespaces {
-		go startWatcher(clientset, &namespace)
-	}
+	var wg sync.WaitGroup
 
-	<-monitorStopped
-	fmt.Fprintf(os.Stdout, "Monitoring stopped")
-}
+	ob := observer.New(&observer.Config{
+		Namespaces: namespaces,
+		Interval:   *interval,
+		KubeConfig: *kubeconfig,
+	})
 
-func startWatcher(clientset *kubernetes.Clientset, namespace *string) {
-	deployments, err := clientset.AppsV1().Deployments(*namespace).List(context.TODO(), metav1.ListOptions{})
+	// create a context that we can cancel
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	if err != nil {
-		panic(err)
-	}
+	ob.Start(ctx, &wg)
 
-	for _, deploy := range deployments.Items {
-		deployVar := deploy
-		go monitorDeployment(clientset, namespace, &deployVar)
-	}
-}
+	<-done
 
-func monitorDeployment(clientset *kubernetes.Clientset, namespace *string, deployment *v1.Deployment) {
-	defer func() {
-		monitorStopped <- struct{}{}
-	}()
+	cancel()
 
-	tick := time.Tick(5 * time.Second)
-	for {
-		select {
-		case <-tick:
-			deployPods, err := clientset.CoreV1().Pods(*namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: "app=" + deployment.Name})
-
-			if err != nil {
-				panic(err)
-			}
-
-		deployLoop:
-			for _, pod := range deployPods.Items {
-				if pod.Status.Phase != "Running" && time.Since(pod.CreationTimestamp.Time) > time.Duration(2*time.Minute) {
-					fmt.Fprintf(os.Stderr, "Deployment: %s in Namespace: %s not ready \n", deployment.Name, *namespace)
-				} else if len(pod.Status.ContainerStatuses) > 0 {
-					for _, container := range pod.Status.ContainerStatuses {
-						if !container.Ready && time.Since(pod.CreationTimestamp.Time) > time.Duration(2*time.Minute) {
-							fmt.Fprintf(os.Stderr, "Deployment: %s in Namespace: %s not ready \n", deployment.Name, *namespace)
-							break deployLoop
-						}
-					}
-				} else {
-					fmt.Fprintf(os.Stderr, "No container running in Deployment: %s in Namespace: %s not ready \n", deployment.Name, *namespace)
-				}
-			}
-		case <-done:
-			fmt.Fprintf(os.Stdout, "Exiting...\n")
-			return
-		}
-	}
+	log.Println("Waiting for all goroutines to finish...")
+	wg.Wait()
+	log.Println("Monitoring stopped.")
 }
